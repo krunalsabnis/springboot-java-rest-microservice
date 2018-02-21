c67d18bee31e6abddf059074b2d0cc2ce14ebaf2
Krunal Sabnis <krunalsabnis@gmail.com>
2018-02-21 14:56:31 +0530

    add gitattribute to override EOL encoding which breaks shell scripts at times. - nginx alpine broke today due to recent release, using stable docker image

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-JAVA_OPTS="-Dfile.encoding=UTF-8 -Djava.security.edg=file:/dev/urandom $JAVA_OPTS"
+#JAVA_OPTS="-Dfile.encoding=UTF-8 -Djava.security.edg=file:/dev/urandom $JAVA_OPTS"
+#JAVA_OPTS="-javaagent:/pinpoint-bootstrap-1.7.0.jar -Dpinpoint.agentId=app-in-docker -Dpinpoint.applicationName=qualibrate-api $JAVA_OPTS"
 
-echo "DBHOST" $DBHOST
 
 echo "starting"
 exec /wait-for-it.sh $DBHOST:3306 -t 0 -- java $JVM_OPTS $JAVA_OPTS -jar app.jar
