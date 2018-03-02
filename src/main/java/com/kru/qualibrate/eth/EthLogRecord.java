cd9afe6465b5a51c086d8349554bbf3b4ab0a00e
Krunal Sabnis <krunalsabnis@gmail.com>
2018-03-02 23:14:46 +0530

    add etherium service - use testnet - log error from eth net into db - solidity contract deployed in test net - use web3J and web3J wrapper for solidity and web3JS

---
+++ src/main/java/com/kru/qualibrate/eth/EthLogRecord.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.kru.qualibrate.eth;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author <a href="mailto:krunalsabnis@gmail.com">Krunal Sabnis</a>
+ *
+ */
+@Data
+@NoArgsConstructor
+@Entity(name="eth_log")
+public class EthLogRecord {
+	@Id
+	@NotNull
+	@Column(name="id")
+	@GeneratedValue
+	private long id;
+
+	private String transaction;
+
+	private String response;
+
+	private Date timestamp = new Date();
+
+	public EthLogRecord(EthDTO eth) {
+		this.transaction = eth.getTransaction();
+		this.response = eth.getResponse();
+		this.timestamp = eth.getTimestamp();		
+	}
+}
---
+++ src/main/java/com/kru/qualibrate/eth/EthLogService.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.kru.qualibrate.eth;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author <a href="mailto:krunalsabnis@gmail.com">Krunal Sabnis</a>
+ *
+ */
+@Service
+public class EthLogService {
+	
+	private EthLogRepository ethLogRepository;
+ 
+	private EthConverter ethConverter;
+
+	public EthLogService(EthLogRepository ethLogRepository,
+			EthConverter ethConverter) {
+		this.ethLogRepository = ethLogRepository;
+		this.ethConverter = ethConverter;
+	}
+
+	public void save(EthDTO ethLog) {
+		ethLogRepository.save(new EthLogRecord(ethLog));
+	}
+
+	public Page<EthDTO> getLogs(Pageable page) {
+		return ethLogRepository.findAll(page).map(ethConverter);
+	}
+}
